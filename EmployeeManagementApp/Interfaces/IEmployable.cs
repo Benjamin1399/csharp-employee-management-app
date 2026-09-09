517e01845d6f1efc84e9a07f08b504929bbe9c17
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-09-09 15:49:58 +0200

    Created test data

---
+++ EmployeeManagementApp/Interfaces/IEmployable.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeManagementApp
+{
+    public interface IEmployable : IPayable
+    {
+        string EmployeeName { get; set; }
+
+        int EmployeeId { get; set; }
+
+        string Department { get; set; }
+
+        decimal HourlyRate { get; set; }
+
+    }
+}
--- EmployeeManagementApp/Program.cs
+++ EmployeeManagementApp/Program.cs
@@ -10,6 +10,36 @@ namespace EmployeeManagementApp
     {
         static void Main(string[] args)
         {
+            var fullTimeEmployee = new FullTimeEmployeeModel()
+            {
+                EmployeeId = 1,
+                EmployeeName = "Benjamin",
+                Department = "IT",
+                HourlyRate = 230.69M
+            };
+
+            Console.WriteLine($"{fullTimeEmployee.EmployeeName} in {fullTimeEmployee.Department} earned R{fullTimeEmployee.CalculateMonthlyPay()}");
+
+            var partTimeEmployee = new PartTimeEmployeeModel
+            {
+                EmployeeId = 2,
+                EmployeeName = "Yashna",
+                Department = "Biology",
+                HourlyRate = 125.56M
+            };
+
+            Console.WriteLine($"{partTimeEmployee.EmployeeName} in {partTimeEmployee.Department} earned R{partTimeEmployee.CalculateMonthlyPay()}");
+
+            var contractor = new ContractorModel
+            {
+                EmployeeId = 2,
+                EmployeeName = "Tim",
+                Department = "Construction",
+                HourlyRate = 86.89M,
+                HoursWorked = 35
+            };
+            Console.WriteLine($"{contractor.EmployeeName} in {contractor.Department} has worked {contractor.HoursWorked} hours and earned R{contractor.CalculateMonthlyPay()}");
+
         }
     }
 }
